9c46b2cb93a4885926d9b06858d3e89f773c055f
Shoshana Szanzer <sszanzer2@student.touro.edu>
2023-09-26 13:37:06 -0400

    Changed the HelloWorld class to be modular when calculating the sum, added the Adder class to calculate the sum

--- src/ss/HelloWorld.java
+++ src/ss/HelloWorld.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter 2 numbers you would like to know the sum of.");
 		
@@ -13,8 +14,9 @@ public class HelloWorld {
 		System.out.println("Please enter the 2nd number: ");
 		int secondNum= keyboard.nextInt();
 		
-		System.out.println("The sum of the two numbers you entered is: " + (firstNum + secondNum));
+		Adder sum = new Adder(firstNum, secondNum);
 		
+		System.out.println("The sum of your two numbers is: " + sum.add(firstNum, secondNum));
 	}
 
 }
