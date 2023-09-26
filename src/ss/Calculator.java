271659aad65d9fa6e3eb1dc7465601817ec75344
Shoshana Szanzer <sszanzer2@student.touro.edu>
2023-09-26 14:29:26 -0400

    Removed the Adder and Subtractor files and added the Calculator file which calculates the sum and difference of the numbers, modified HelloWorld to use calculator class and added a do while loop and added if statement allowing user to choose whether it wants to add or sub the numbers it input

--- src/ss/Calculator.java
+++ src/ss/Calculator.java
@@ -1,14 +1,20 @@
 package ss;
 
-public class Subtractor {
+public class Calculator {
 	private int num1;
 	private int num2;
+	private int sum;
 	private int difference;
 	
-	public Subtractor(int num1, int num2) {
+	public Calculator (int num1, int num2) {
 		this.num1=num1;
 		this.num2=num2;
-		
+	}
+	
+	
+	public int add(int num1, int num2) {
+		sum = num1+num2;
+		return sum;
 	}
 	
 	public int subtract(int num1, int num2) {
--- src/ss/HelloWorld.java
+++ src/ss/HelloWorld.java
@@ -5,21 +5,42 @@ import java.util.*;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
+		
+		boolean cont= true;
 		Scanner keyboard = new Scanner(System.in);
+		
+		do {
+		try {	
+		
 		System.out.println("Enter 2 numbers you would like to know the sum of.");
 		
 		System.out.println("Please enter the 1st number: ");
 		int firstNum = keyboard.nextInt();
 		System.out.println("Please enter the 2nd number: ");
 		int secondNum= keyboard.nextInt();
+		keyboard.nextLine();
 		
-		Adder sum = new Adder(firstNum, secondNum);
-		Subtractor sub = new Subtractor(firstNum, secondNum);
+		Calculator calc = new Calculator(firstNum, secondNum);
 		
-		System.out.println("The sum of your two numbers is: " + sum.add(firstNum, secondNum));
+		System.out.println("Would you like to add or subtract the numbers? Type 'add' for add or 'sub' for subtract.");
+		String response= keyboard.nextLine().toLowerCase();
 		
-		System.out.println("The difference of the two numbers is: " +sub.subtract(firstNum, secondNum));
-	}
+		 if (response.equals("add"))
+			 System.out.println("The sum of your two numbers is: " + calc.add(firstNum, secondNum));
+		 else if( response.equals("sub"))
+			 System.out.println("The difference of the two numbers is: " + calc.subtract(firstNum, secondNum));
+		 
+		
+		System.out.println("Would you like to calculate other numbers? If yes type Yes if no type No: ");
+		String answer = keyboard.nextLine().toUpperCase();
+		
+		if(answer.equals("NO"))
+			cont= false;
+		}catch(InputMismatchException i) {
+			System.out.println("Incorrect input type. Please try running the program again.");
+		}
+		
+	} while(cont);
 
-}
+	}
+}	
