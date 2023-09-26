836194bf3f4609a20af77af5f30e4991b32688bb
Shoshana Szanzer <sszanzer2@student.touro.edu>
2023-09-26 13:20:22 -0400

    Changed the document from saying hello world to asking for 2 numbers and then displaying the sum of the two numbers

--- src/ss/HelloWorld.java
+++ src/ss/HelloWorld.java
@@ -1,9 +1,19 @@
 package ss;
 
+import java.util.*;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter 2 numbers you would like to know the sum of.");
+		
+		System.out.println("Please enter the 1st number: ");
+		int firstNum = keyboard.nextInt();
+		System.out.println("Please enter the 2nd number: ");
+		int secondNum= keyboard.nextInt();
+		
+		System.out.println("The sum of the two numbers you entered is: " + (firstNum + secondNum));
 
 	}
 
