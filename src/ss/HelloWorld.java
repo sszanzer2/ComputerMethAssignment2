52e334e1ec7d6bdbb6d9448d070f537b89f85741
sszanzer2 <122063650+sszanzer2@users.noreply.github.com>
2023-09-26 13:24:03 -0400

    removed the extra documentation

--- src/ss/HelloWorld.java
+++ src/ss/HelloWorld.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter 2 numbers you would like to know the sum of.");
 		
@@ -15,10 +14,7 @@ public class HelloWorld {
 		int secondNum= keyboard.nextInt();
 		
 		System.out.println("The sum of the two numbers you entered is: " + (firstNum + secondNum));
-=======
-		System.out.println("Hello World");
->>>>>>> 958544929abe8fbfca3451652e91eb96f9027d58
-
+		
 	}
 
 }
